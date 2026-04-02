d0cf4b56746174ec0cdd2c9952caa6372cfe4817
Rakibul-E404 <smrat246@gmail.com>
2026-04-02 13:02:38 +0600

    base frontend done__

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,5 @@
-/**
-
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:subliminal/visual/main_bottom_nav/main_bottom_nav_screen.dart';
-import 'visual/splash/splash_screen.dart';
-
-
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Free Trial App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      // home: const SplashScreen(),
-      home: const MainBottomNavScreen(),
-    );
-  }
-}
-*/
-
-
-
-
-
-///
-///
-///
-///
-/// todO:: adding flash
-///
-///
-///
-
-
-
-import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:system_alert_window/system_alert_window.dart';
 import 'visual/main_bottom_nav/main_bottom_nav_screen.dart';
 import 'visual/settings/settings_dialogue.dart';
 
